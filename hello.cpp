876a1c67424a9fdb8b50cfded4e542c2346879c2
fusionchan <xiaoc90@qq.com>
2018-06-20 19:43:36 +0800

    a

--- hello.cpp
+++ hello.cpp
@@ -2,8 +2,13 @@
 #include<stdio.h>
 #include<algorithm>
 using namespace std;
+int a,b;
+int asdffg;
 int main()
 {
+  int x,y;
+  cin>>x>>y;
+  cout<<x+y<<endl;
   cout<<"hello world!"<<endl;
   return 0;
   // zhushi 1

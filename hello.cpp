b069b7c166b2dacf5ba3305c3aa0af887010c515
fusionchan <xiaoc90@qq.com>
2018-06-21 11:40:33 +0800

    1139

--- hello.cpp
+++ hello.cpp
@@ -6,15 +6,5 @@ int a,b;
 int asdffg;
 int main()
 {
-  int x,y;
-  cin>>x>>y;
-  cout<<x+y<<endl;
-  cout<<"hello world!"<<endl;
-  cout <<"hellotest"<<endl;
-  return 0;
-  // zhushi 1
-  // zhushi 2
-  // aaaa
-  // aaaddd
-  // sss
+	1139
 }
